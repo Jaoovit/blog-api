e64a8fcfd15e02c0a1555e1f6de379acf7e80a05
João Oliveira <jaoovit@gmail.com>
2024-10-01 18:37:24 +0100

    create a function to create post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,46 @@
+require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const prisma = new PrismaClient();
 
-module.exports = {};
+const createPost = (req, res) => {
+  jwt.verify(req.token, JWT_SECRET, async (err) => {
+    if (err) {
+      res
+        .sendStatus(403)
+        .json({ message: "Without autorization to create posts" });
+    } else {
+      const { title, postContent } = req.body;
+
+      const userId = req.session.passport.user;
+
+      if (!title || !postContent) {
+        return res
+          .status(400)
+          .json({ message: "Title and content are required." });
+      }
+      try {
+        const newPost = await prisma.post.create({
+          data: {
+            title: title,
+            content: postContent,
+            user: {
+              connect: { id: userId },
+            },
+          },
+        });
+        res.json({
+          message: "Post created...",
+          post: newPost,
+        });
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error creating post." });
+      }
+    }
+  });
+};
+
+module.exports = { createPost };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const postController = require("../controllers/postController");
 const router = express.Router();
+const verifyToken = require("../config/token");
+
+router.post("/post", verifyToken, postController.createPost);
 
 module.exports = router;
